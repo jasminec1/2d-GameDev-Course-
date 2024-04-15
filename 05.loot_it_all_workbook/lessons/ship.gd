4759730b92e51646733327fc980d36f8b43012c3
jasminec1 <jacruz9@s.sfusd.edu>
2024-04-15 12:04:55 -0700

    L4

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -19,5 +19,6 @@ func _process(delta: float) -> void:
 	velocity += steering * steering_factor * delta
 	position += velocity * delta
 
+
 	if velocity.length() > 0.0:
-		rotation = velocity.angle()
+		get_node("Sprite2D").rotation = velocity.angle()
