d4342cda30a8260a1b97fc7bedc00bf32b7de00a
jasminec1 <jacruz9@s.sfusd.edu>
2024-04-29 11:22:02 -0700

    L3 P2 and L4

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -9,8 +9,7 @@ func _ready() -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
 
 func set_outline_thickness(new_thickness: float) -> void:
-	if canvas_group_has_material:
-		canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
 
 func _on_mouse_entered() -> void:
 	var tween := create_tween()
@@ -19,3 +18,18 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
+	
+func _input_event(viewport: Node, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func open() -> void:
+	animation_player.play("open")
